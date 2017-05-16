a92f6363436ca70eaf65a162bc7621a9a013c082
Zane Russell <zaneroosa@gmail.com>
2017-05-16 11:52:42 -0600

    data scrubber for new films

--- app/helpers/getNewFilms.js
+++ app/helpers/getNewFilms.js
@@ -2,7 +2,15 @@ const getNewFilms = () => {
   let p1 = apiCall('https://api.themoviedb.org/3/movie/now_playing?api_key=943e4f5ccf1cdbbcab342f134a46713a&language=en-US&page=1')
 
   return Promise.all([p1]).then(filmArray => {
-    return filmArray[0].results;
+    return filmArray[0].results.map((val, index) => {
+      let movieHolder = {}
+      movieHolder.title = val.title
+      movieHolder.vote_average = val.vote_average
+      movieHolder.vote_count = val.vote_count
+      movieHolder.poster_path = val.poster_path
+      movieHolder.overview = val.overview
+      return movieHolder
+    })
   })
 }
 
