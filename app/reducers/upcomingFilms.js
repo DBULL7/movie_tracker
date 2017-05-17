1c025bcc6fd22f9c37e7f759356e1c6392637c7e
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-05-17 14:34:32 -0600

    trying to pass props

--- app/reducers/upcomingFilms.js
+++ app/reducers/upcomingFilms.js
@@ -1,10 +1,20 @@
 const upcomingFilms = (state = [], action) => {
   switch (action.type) {
     case 'GET_MOVIES':
-      return action.payload.results
+      return action.payload.results.map((val, index) => {
+        let movieHolder = {}
+        movieHolder.title = val.title
+        movieHolder.vote_average = val.vote_average
+        movieHolder.vote_count = val.vote_count
+        movieHolder.poster_path = val.poster_path
+        movieHolder.overview = val.overview
+        movieHolder.id = val.id
+        movieHolder.vote_average = val.vote_average
+        return movieHolder
+      })
     default:
       return state
+    }
   }
-}
 
 export default upcomingFilms
