aa5847e0bb82faae83b06041bbfcb0c28ae58e54
Devon Bull <dbull@live.com>
2017-05-17 16:26:07 -0600

    finally fixed?

--- app/components/AppContainer.js
+++ app/components/AppContainer.js
@@ -3,16 +3,7 @@ import App from './app';
 import { upcomingFilms } from '../actions/index'
 
 function mapStateToProps(state) {
-<<<<<<< HEAD
-  console.log(state);
-  const newArray = [...state];
-  console.log(newArray);
-  let test = Object.assign({}, state, newArray)
-  console.log(test);
-  return test
-=======
   return { movies: state.movies }
->>>>>>> 26a99b3e7c56628b7c1902319148dadcbdb935b0
 }
 
 function mapDispatchToProps(dispatch) {
