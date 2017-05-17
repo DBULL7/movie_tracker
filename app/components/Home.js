9c91b91a2a5950554b4ed36891271f0624555b2e
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-05-17 16:10:44 -0600

    display all movie info

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Movie } from './movieCard'
 
 export const Home = ({upcomingFilms}) => {
+  console.log(upcomingFilms);
   return(
     <section>
       <h2 className="home-title">Home</h2>
