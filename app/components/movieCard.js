b2b6e36f922aaa94c2df36a10f529ba6e24a95e8
Devon Bull <dbull@live.com>
2017-05-19 00:08:48 -0600

    noticed an fixed small bug, don't see any css changes

--- app/components/movieCard.js
+++ app/components/movieCard.js
@@ -6,13 +6,15 @@ export const Movie = (props) => {
   const film = props
   const { title, overview, release_date, poster_path, getFav, handleClick, displayMovie } = props
   return (
-    <article onClick={() => displayMovie(film)}  className='movie'>
-      <h3 className="movie-content">{title}</h3>
-      <img className="movie-content" src={`https://image.tmdb.org/t/p/w300/${poster_path}`} />
-      <p className="movie-content">{release_date}</p>
-      <p className="movie-content overview">{overview}</p>
-      <button className='favorite' onClick={(e) => {
-                                    getFav(film); handleClick(film)}}>fav</button>
+    <article className='movie'>
+      <article onClick={() => displayMovie(film)}>
+        <h3 className="movie-content">{title}</h3>
+        <img className="movie-content" src={`https://image.tmdb.org/t/p/w300/${poster_path}`} />
+        <p className="movie-content">{release_date}</p>
+        <p className="movie-content overview">{overview}</p>
+      </article>
+        <button className='favorite' onClick={(e) => {
+          getFav(film); handleClick(film)}}>fav</button>
     </article>
   );
 };
