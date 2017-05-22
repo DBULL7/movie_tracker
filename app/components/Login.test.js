acacf4de922fe7d07409a2787bd20feceeb95b94
Devon Bull <dbull@live.com>
2017-05-21 19:34:27 -0600

    Login testing finally starting to work

--- app/components/Login.test.js
+++ app/components/Login.test.js
@@ -4,29 +4,39 @@ import { Provider } from 'react-redux';
 import configureMockStore from 'redux-mock-store';
 import LoginContainer from './LoginContainer';
 import Login from './Login';
-import rootReducer from '../reducers/rootReducer'
 import fetchMock from 'fetch-mock'
 
-describe('Login Component Tests', () => {
-  it('it should shallow mount without crashing', () => {
-    const storageMock = () => {
-    let storage = {}
-     return {
-       user: 'bob',
-       clear: function() {
-         return this.user = ''
-       },
-       getItem: function(key) {
-         return key in storage ? storage[key] : null;
-       },
-     }
-   }
 
-   window.localStorage = storageMock()
-    const mockStore = configureMockStore()({
-      loginUser: {id: 1, email: 'dbull', password: 'ya'}
-    })
-    console.log(mockStore)
-    const wrapper = mount(<Provider store={mockStore}><LoginContainer/></Provider>)
+const storageMock = () => {
+let storage = {}
+ return {
+   user: 'bob',
+   clear: function() {
+     return this.user = ''
+   },
+   getItem: function(key) {
+     return key in storage ? storage[key] : null;
+   },
+ }
+}
+
+window.localStorage = storageMock()
+
+const mockStore = configureMockStore()({
+  loginUser: {id: 2, email: 'dbull', password: 'ya'}
+})
+
+
+describe('Login Component Tests', () => {
+  it('it should mount without crashing', () => {
+
+    const wrapper = mount(<Provider store={mockStore}><LoginContainer history={'http://localhost:3000/'}/></Provider>)
+  })
+
+  it('should have empty strings for states', () => {
+    const wrapper = mount(<Provider store={mockStore}><LoginContainer history={'http://localhost:3000/'}/></Provider>)
+    const test = wrapper.find(Login)
+
+    expect(test.node.state.email).toEqual('')
   })
 })
